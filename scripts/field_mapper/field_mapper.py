d116b144adf0a7984792eafce72e8e29a3520010
Narek <narek.saribekyan@ing.com>
2018-09-25 18:28:19 +0200

    Field mapper

--- scripts/field_mapper/field_mapper.py
+++ scripts/field_mapper/field_mapper.py
@@ -1,13 +1,35 @@
 #!/bin/python
-import sys 
 import argparse
 
-parser = argparse.ArgumentParser(description="Maps field names from one from file to the other")
+parser = argparse.ArgumentParser(description="Maps field names from one file to another")
 
-parser.add_argument("old-fields-file", type=file)
-parser.add_argument("new-fields-file", type=file)
+parser.add_argument("old_fields_file")
+parser.add_argument("new_fields_file")
 
-parser.parse_args()
+args = parser.parse_args()
+old_fields = open(args.old_fields_file, "r").read().splitlines()
+new_fields = open(args.new_fields_file, "r").read().splitlines()
+
+mapping = {}
+
+for old_field in old_fields:
+    mapping[old_field] = "N/A"
+    for new_field in new_fields:
+        if new_field.find(old_field) != -1:
+            mapping[old_field] = new_field
+            break
+
+#print(mapping)
+
+
+for k,v in mapping.items():
+    print(k + ", " + v)
+
+
+
+
+
+
 
 
 
